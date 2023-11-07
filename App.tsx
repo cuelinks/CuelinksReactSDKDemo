b5dbaa0e1af3be5325e276678bb72a96539bda90
Nikhil Sawant <nikhilsawant@Dharatis-MacBook-Pro.local>
2023-11-07 16:19:13 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React,{useState} from 'react';
+import type {PropsWithChildren} from 'react';
+import CueLinkModule from './CueLinkModule';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  Button,
+  useColorScheme,
+  TouchableOpacity,
+  View,
+  Linking,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  const [valueFromNativeModule, setValueFromNativeModule] = useState(null);
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const handleLinkClick = async (url) => {
+     const modifiedLink = await CueLinkModule.getAffiliateLink(url);
+     setValueFromNativeModule(modifiedLink)
+//      Linking.openURL(modifiedLink);
+
+    };
+
+  return (
+        <View style={styles.centered}>
+        <TouchableOpacity onPress={() => handleLinkClick('https://example.com')}>
+                <Text style={styles.sectionTitle}>Click Here To Generate Test Link!</Text>
+                <Text/>
+                <Text style={styles.highlight}>Generated Link  :</Text>
+
+                <Text >{valueFromNativeModule}</Text>
+        </TouchableOpacity>
+        </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  centered: {
+      flex : 1,
+      justifyContent: "center",
+      alignItems: "center",
+  },
+});
+
+export default App;
